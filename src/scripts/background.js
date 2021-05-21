b5f8801ea6de493dea2cff7cce0693d8a2ce41fd
asmape0104 <6086632642@g.ecc.u-tokyo.ac.jp>
2021-05-21 17:51:14 +0900

    add: credintial omit

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -26,7 +26,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         });
       return true;
     case 'watchdata':
-      fetch(`https://www.nicovideo.jp/watch/${request.contentId}`)
+      fetch(`https://www.nicovideo.jp/watch/${request.contentId}`, {
+        credentials: 'omit'
+      })
             .then(res => res.text())
             .then(html => {
               const matchArr = html.match(/id="js-initial-watch-data" data-api-data="([^"]+)"/);
